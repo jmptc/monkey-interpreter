b45d371860b283db6dcdd550619aee3320c74f7e
jmptc <carner1024@gmail.com>
2022-06-08 23:27:23 -0500

    added repl

--- main.go
+++ main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"github.com/jmptc/monkey-interpreter/repl"
+	"os"
+	"os/user"
 )
 
 func main() {
-	fmt.Println("Hello monkey interpreter!")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n",
+		user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
