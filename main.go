96d70885b0d6e31f2490d6a77cc511f6c5c240b5
jmptc <carner1024@gmail.com>
2022-06-20 01:43:20 -0500

    parsing done :)

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+    "github.com/jmptc/monkey-interpreter/lexer"
+    "github.com/jmptc/monkey-interpreter/parser"
 	"github.com/jmptc/monkey-interpreter/repl"
 	"os"
 	"os/user"
@@ -12,8 +14,19 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Hello %s! This is the Monkey programming language!\n",
-		user.Username)
-	fmt.Printf("Feel free to type in commands\n")
-	repl.Start(os.Stdin, os.Stdout)
+
+    // temp for testing
+    input := "let x = 1 + 2;"
+
+    if input != "" {
+        l := lexer.New(input)
+        p := parser.New(l)
+        program := p.ParseProgram()
+        fmt.Println(program.String())
+    } else {
+        fmt.Printf("Hello %s! This is the Monkey programming language!\n",
+            user.Username)
+        fmt.Printf("Feel free to type in commands\n")
+        repl.Start(os.Stdin, os.Stdout)
+    }
 }
